ffab7018f294c8df33ceb4eee0ec4497b602a5ee
bhandhariya <sainiraja59@gmail.com>
2019-06-30 16:45:53 +0530

    register function added

--- src/app/services/register.service.ts
+++ src/app/services/register.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { ConfigService } from '../shared/config.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Injectable()
 export class RegisterService {
     private base_url: string;
-    constructor(private configService:ConfigService, private http: HttpClient) {
+    constructor(private configService:ConfigService, private http: HttpClient,private auth:AngularFireAuth,private router:Router) {
         this.base_url = configService.getBaseUrl();
      }
 
@@ -22,5 +25,38 @@ export class RegisterService {
               })}
           return this.http.get("https://ajith-Verify-email-address-v1.p.rapidapi.com/varifyEmail?email="+email, head );
       }
-
-}
+      register(u){
+        console.log(u);
+        this.auth.auth.createUserWithEmailAndPassword(u.Email,u.Password).then(()=>{
+            this.auth.auth.currentUser.updateProfile({
+              displayName:u.FirstName+' '+u.LastName
+            }).then(()=>{
+              var user=this.auth.auth.currentUser;
+              var obj={
+               name:user.displayName,
+               email:user.email,
+               providerId:user.providerId,
+               uid:user.uid,
+               emailVerified:user.emailVerified
+               
+              }
+              
+             this.http.post('https://digitalapp001.herokuapp.com/api/users/register',obj).subscribe(this.cb)
+            })
+       
+            
+            
+          }).catch(error=>{
+           var errorCode = error.code;
+           var errorMessage = error.message;
+           Swal.fire("Oops!", errorMessage, "error");
+          })
+         }
+         cb=(dt)=>{
+          
+           if(dt.uid){
+             Swal.fire("Great", "User Registered Successfully", "success");
+             this.router.navigate(['login'])
+           }
+         }
+      }
