640bac0846c61e4de02a4de3e2dd63077a8bddb1
bhandhariya <sainiraja59@gmail.com>
2019-06-30 16:44:56 +0530

    register function changed to sevice

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 import {RegisterService } from '../services/register.service'
 import Swal from 'sweetalert2';
 import { AngularFireAuth } from "@angular/fire/auth";
-import * as firebase from "firebase";
+
 
 
 @Component({
@@ -27,40 +27,13 @@ export class RegisterComponent implements OnInit {
     this.submmited = true;
     if(this.formCtrl.valid){
       this.loading = true;
-      console.log(formData)
-      this.auth.auth.createUserWithEmailAndPassword(formData.Email,formData.Password).then(res=>{
-        res.user.sendEmailVerification().then(function(){
-          firebase.database().ref('users/'+res.user.uid).set({
-            email:formData.Email,
-            uid:res.user.uid,
-            registrationDate:new Date().toString(),
-            name:formData.FirstName
-          })
-          
-          Swal.fire({type: 'success',title: 'Registerd Successfully',showConfirmButton: false,timer: 1000});
-         
-        });
-        var uu=this.auth.auth.currentUser;
-        uu.updateProfile({
-          displayName:formData.FirstName
-        })
-        this.router.navigate(['login']);
-      },err=>{
-        this.loading = false;
-        Swal.fire({type: 'error',title: 'Registeration not Successfull',showConfirmButton: false,timer: 1500});
-      })
-      // this.registerService.checkEmailExistance(formData.Email).subscribe(res =>{
-      //   if(res['exist'] == 'true'){
-      //     this.registerService.registraction(formData).subscribe(res =>{
-      //       this.loading = false;
-      //       Swal.fire({type :'success', title :'User Created Successfully', showConfirmButton: false, timer:1000});
-      //       this.router.navigate(['login']);
-      //     },err =>this.loading = false)
-      //   }else{
-      //     this.loading = false;
-      //     Swal.fire({type :'error', title :"Email doesn't exist ",text :'This action is checking your email exist in real word on not.', showConfirmButton: false, timer:1500});
-      //   }
-      // });
+      if(formData.Password != formData.ConfirmPassword){
+        Swal.fire('Password Does not Match')
+      }else{
+        this.registerService.register(formData);
+      }
+    }else{
+      Swal.fire('Please entar All Details')
     }
   }
 
